7165b73dd6726675993eca46a281637712ad90ce
ShubhamForYou(Personal) <shubham.devforyou@gmail.com>
2026-04-04 09:52:51 +0530

    feat(category): add create category DTO

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCategoryDto {}
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  name: string;
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  description?: string;
+}
