a849b1bce668edb500e2f178edd4ebeee564ff91
ShubhamForYou(Personal) <shubham.devforyou@gmail.com>
2026-04-09 18:28:03 +0530

    feat(product): add create product endpoints

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@Controller('product')
+@UseGuards(AuthGuard)
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createProductDto: CreateProductDto) {
+    const categoryExists = await this.prisma.category.findFirst({
+      where: { id: createProductDto.categoryId, isDeleted: false },
+    });
+    if (!categoryExists) {
+      throw new NotFoundException(`Category not found`);
+    }
+    const newProduct = await this.prisma.product.create({
+      data: {
+        name: createProductDto.name,
+        description: createProductDto.description,
+        price: createProductDto.price,
+        stock: createProductDto.stock,
+        categoryId: createProductDto.categoryId,
+      },
+    });
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: `Product created successfully`,
+      data: newProduct,
+    };
   }
 
   findAll() {
