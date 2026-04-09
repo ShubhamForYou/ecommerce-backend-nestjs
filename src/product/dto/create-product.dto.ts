060177530172c6770d951d7d3f019da034fc2783
ShubhamForYou(Personal) <shubham.devforyou@gmail.com>
2026-04-09 10:57:45 +0530

    feat(product): add create product DTO

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,31 @@
-export class CreateProductDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.toLowerCase().trim())
+  name: string;
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  description?: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @Transform(({value}) => parseFloat(value))
+  price: number;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({value}) => Number(value))
+  stock: number;
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  categoryId: string;
+}
